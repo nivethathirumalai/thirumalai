7bdf77a9f5a07cd8f6c0c6ee535952c7939fba04
nivethathirumalai <nivethahanshi@gmail.com>
2023-01-31 13:32:12 +0530

    my framework

---
+++ src/test/java/Maven.java
@@ -0,0 +1,38 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+
+import org.apache.poi.ss.usermodel.Cell;
+
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+
+public class Maven {
+public static void main(String[] args) throws IOException{
+	File loc=new File("C:\\Users\\Karjan\\NewWorkspace\\Framework\\Excel\\excel1.xlsx");
+	FileInputStream f=new FileInputStream(loc);
+	Workbook w=new XSSFWorkbook(f);
+    Sheet s=w.getSheet("Sheet1");
+    
+    Row r=s.getRow(5);
+    Cell c=r.getCell(1);
+    String value = c.getStringCellValue();
+    if(value.contains("thirumalai"))
+    		{
+    	c.setCellValue("thirumalaisamy");
+    		}
+    FileOutputStream ff=new FileOutputStream(loc);
+    w.write(ff);
+    System.out.println("done");
+    System.out.println(c);
+		}
+	
+		
+	}
+
+
